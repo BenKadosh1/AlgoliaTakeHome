161830fb2050d80382b4a92aee708051dbfeb97e
BenKadosh1 <ben.n.kadosh@gmail.com>
2020-10-22 16:47:38 -0400

    Updates

---
+++ precache-manifest.9c2a09c210d6c20e4944f2abb209f5d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaf91ea5648eacf8fc2084bf8554aee2",
+    "url": "/AlgoliaTakeHome/index.html"
+  },
+  {
+    "revision": "d41b7f08a2fc3e006f33",
+    "url": "/AlgoliaTakeHome/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "f18243caf404fbd3aebe",
+    "url": "/AlgoliaTakeHome/static/css/main.b888b54a.chunk.css"
+  },
+  {
+    "revision": "d41b7f08a2fc3e006f33",
+    "url": "/AlgoliaTakeHome/static/js/2.0fae7d01.chunk.js"
+  },
+  {
+    "revision": "bf80f4f237e8899f0997ee45b3cc0db5",
+    "url": "/AlgoliaTakeHome/static/js/2.0fae7d01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f18243caf404fbd3aebe",
+    "url": "/AlgoliaTakeHome/static/js/main.8ab9081b.chunk.js"
+  },
+  {
+    "revision": "21535be585c3de5c66dc",
+    "url": "/AlgoliaTakeHome/static/js/runtime-main.652b7829.js"
+  }
+]);
